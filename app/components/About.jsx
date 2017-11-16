6be25a49730f16b8ae4c428cb1bda20fe3b5e9a8
Sorogovets.Nikolay <sorogovets.nikolay@gmail.com>
2017-11-16 22:50:05 +0300

    Update about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
 
-// class About extends Component {
-//   render() {
-//     return (
-//       <h3>About Component</h3>
-//     );
-//   }
-// }
-
 const About = (props) => (
-  <h3>About Component</h3>
+  <div>
+    <h3>About Component</h3>
+    <p>Welcome to about page!</p>
+  </div>
 );
 
 export default About;
