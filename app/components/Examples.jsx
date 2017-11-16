8b4751c439c39fe2aeabf0be7987a84ee6fafc42
Sorogovets.Nikolay <sorogovets.nikolay@gmail.com>
2017-11-16 22:15:06 +0300

    Updated examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const Examples = (props) => (
+  <div>
   <h3>Examples Component stateless</h3>
+  <p>Welcome to examples page!</p>
+  </div>
 )
 
 export default Examples;
