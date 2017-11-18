96b76e54d610ab54d5828cd0b95b65c7672de6e8
Sorogovets.Nikolay <sorogovets.nikolay@gmail.com>
2017-11-18 15:49:15 +0300

    Updated nav styles

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,25 +1,56 @@
 import React from 'react';
 import { Link, IndexLink } from 'react-router';
 
-const Nav = (props) => (
-  <div className='nav'>
-    <h2>Nav Component</h2>
-    <IndexLink
-      to='/'
-      activeClassName="active"
-      activeStyle={{ fontWeight: 'bold' }}
-    >Get Weather</IndexLink>
-    <Link
-      to='/about'
-      activeClassName="active"
-      activeStyle={{ fontWeight: 'bold' }}
-    >About</Link>
-    <Link
-      to="/examples"
-      activeClassName="active"
-      activeStyle={{ fontWeight: 'bold' }}
-    >Examples</Link>
-  </div>
-);
+class Nav extends React.Component {
+  onSearch = (e) => {
+    e.preventDefault();
+    alert("Not implemented yet");
+  }
+
+  render() {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Weather</li>
+            <li>
+              <IndexLink
+                to='/'
+                activeClassName="active"
+                activeStyle={{ fontWeight: 'bold' }}
+              >Get Weather</IndexLink>
+            </li>
+            <li>
+              <Link
+                to='/about'
+                activeClassName="active"
+                activeStyle={{ fontWeight: 'bold' }}
+              >About</Link>
+            </li>
+            <li>
+              <Link
+                to="/examples"
+                activeClassName="active"
+                activeStyle={{ fontWeight: 'bold' }}
+              >Examples</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-rigth">
+          <form onSubmit={this.onSearch}>
+            <ul className="menu">
+              <li>
+                <input type="search" placeholder="Search weather" />
+              </li>
+              <li>
+                <button className="button">Get Weather</button>
+              </li>
+            </ul>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Nav;
